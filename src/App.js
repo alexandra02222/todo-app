68abd57a099986e6aa728d2f9a913b73e4948b74
Yuno0 <yunokaznoqtie@gmail.com>
2026-04-18 19:06:32 +0800

    russ

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import {
-  AppContainer,
-  InputField,
-  AddButton,
-  TodoItem,
-  DeleteButton
-} from './components/StyledComponents';
+import React, { useState } from 'react';
+import { AppContainer, InputField, AddButton, TodoItem, DeleteButton } from './components/StyledComponents';
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([
+        ...todos,
+        { id: Date.now(), text: newTodo, completed: false }
+      ]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
     <AppContainer>
       <h1>To-Do List</h1>
@@ -15,16 +34,42 @@ const TodoApp = () => {
       <div>
         <InputField
           type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
           placeholder="Enter a new to-do"
         />
-        <AddButton>Add To-Do</AddButton>
+        <AddButton onClick={addTodo}>Add To-Do</AddButton>
       </div>
 
       <div>
-        <TodoItem>
-          <span>Sample To-Do</span>
-          <DeleteButton>Delete</DeleteButton>
-        </TodoItem>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} completed={todo.completed}>
+            
+            {/* Checkbox for marking complete */}
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
+            />
+
+            {/* Todo text */}
+            <span
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+                marginLeft: '8px',
+                cursor: 'pointer'
+              }}
+              onClick={() => toggleComplete(todo.id)}
+            >
+              {todo.text}
+            </span>
+
+            <DeleteButton onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </DeleteButton>
+
+          </TodoItem>
+        ))}
       </div>
     </AppContainer>
   );
